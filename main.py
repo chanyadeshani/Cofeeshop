a1ae248f93a0ee4abe1a4ecfe50724edc1d692be
chanya.deshani <chanya.deshani@gmail.com>
2024-01-17 16:55:03 +0000

    Add items to shoping cart

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-
-@app.route('/second_page')
-def secondpage():
-    return render_template('second_page.html')
+app.config['SECRET_KEY'] = 'Some hard to guess key'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['Mail_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 
-@app.route('/user/<username>')
-def show_user(username):
-    # Greet the user
-    return 'Hello ' + username
-gender = "Male"
-gender = gender.capitalize().strip()
-@app.route('/menu/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('login.html', form=form, name=name)
+
+
+@app.route('/menu')
 def menu():
-    products = {"Espresso":["Latte","Mocha","Macchiato","Cappuccino","Americano"],
-                "Brewed Coffee":["Filter coffee", "Iced Coffee & Cold Brew"],
-                "Hot Tea":["Brewed Tea","Tea Latte"],
-                "Iced Tea":["Iced Tea","Iced Tea Latte"],
-                "Hot Chocolate":["Classic Hot Chocolate","White Hot Chocolate"]
+    products = {"Espresso": [["Latte", 3.0], ["Mocha", 4.0], ["Macchiato", 2], ["Cappuccino", 4], ["Americano", 5]],
+                "Brewed Coffee": [["Filter coffee", 2], ["Iced Coffee & Cold Brew", 3]]
                 }
 
     return render_template('menu.html', products=products)
